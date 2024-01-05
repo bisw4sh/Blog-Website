10331de51cae16c1077dbc0c125557815d696936
Biswash <spikeystona@gmail.com>
2024-01-05 11:46:50 +0545

    fixed obj in children -> rendring issue

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 type Props = {
   title: string;
@@ -10,26 +10,47 @@ type Props = {
   content: string;
 };
 
-export default function Blog({ title, author, category, img, date, content }: Props) {
+export default function Blog({
+  title,
+  author,
+  category,
+  img,
+  date,
+  content,
+}: Props) {
+  const spacedContent = content.replace(/#(\w+)/g, "# $1");
+  const sections = spacedContent.split("#").filter(Boolean);
   return (
-    <div>
-      {/* <Image
-        src={`/img/${src}`}
-        alt={alt}
-        height={400}
-        width={400}
-        className="rounded-xl"
-      /> */}
+    <div className="bg-zinc-800 mt-4 rounded-lg p-4 flex flex-col lg:w-1/2 justify-center items-center">
+      <Image
+        src={`/img/${img}`}
+        priority
+        alt={img}
+        height={200}
+        width={200}
+        className="rounded-xl "
+        style={{ height: "auto", width: "auto" }}
+      />
 
-      <li key={title}>
-        {/* Render data properties here */}
-        <p>Title: {title}</p>
+      <div key={title}>
+        <p className="text-xl font-semibold">{title}</p>
         <p>Author: {author}</p>
-        <p>Category: {category}</p>
-        <p>Image: {img}</p>
-        <p>Date: {date}</p>
-        <p>Content: {content}</p>
-      </li>
+        <p>Date: {date.toString()}</p>
+        <div>
+          {sections.map((section, index) => {
+            const [header, ...paragraphs] = section.trim().split("\n");
+            return (
+              <section key={index}>
+                <h1>{header.trim()}</h1>
+                <p>{paragraphs.join("\n").trim()}</p>
+              </section>
+            );
+          })}
+        </div>
+      </div>
+      <p className="self-start py-[2px] px-2 rounded-lg bg-cyan-50 text-violet-900 text-md">
+        {category}
+      </p>
     </div>
   );
 }
